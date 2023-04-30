da57b83995e3a6999c06dc1be4a20ca52fed17e1
Dmytro Moroz <frostdimonlex@gmail.com>
2023-04-30 21:05:33 +0300

    add variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
 }
   provider "google" {
-    project = "infra-384407"
-    region = "europe-west-1"
+    project = "${var.project}"
+    region = "${var.region}"
 }
 resource "google_compute_instance" "app" {
   name = "reddit-app"
@@ -10,26 +10,42 @@ resource "google_compute_instance" "app" {
   zone = "europe-west1-b"
   tags = ["reddit-app"]
   metadata = {
-    ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
+    ssh-keys = "appuser:${file(var.public_key_path)}\nappuser1:${file(var.public_key_path)}"
   }
 boot_disk {
   initialize_params {
-    image = "reddit-base-1682758412"
+    image = "${var.disk_image}"
   }
 }
 network_interface {
   network = "default"
 access_config {}
 }
+connection {
+  type = "ssh"
+  host = self.network_interface.0.access_config.0.nat_ip
+  user = "appuser"
+  agent = false
+  private_key = "${file("~/.ssh/appuser")}"
+}
+
+provisioner "file" {
+  source = "files/puma.service"
+  destination = "/tmp/puma.service"
+}
+
+provisioner "remote-exec" {
+  script = "files/deploy.sh"
+}
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-name = "allow-puma-default"
-network = "default"
-allow {
-protocol = "tcp"
-ports = ["9292"]
+  name = "allow-puma-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = ["9292"]
 }
-source_ranges = ["0.0.0.0/0"]
-target_tags = ["reddit-app"]
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["reddit-app"]
 }
