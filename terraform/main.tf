61ec82b5691e12398309093bbec10e4a29360b1c
Dmytro Moroz <frostdimonlex@gmail.com>
2023-05-03 09:34:42 +0300

    add firewall rules to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,9 @@ boot_disk {
 }
 network_interface {
   network = "default"
-access_config {}
+  access_config {
+    nat_ip = "${google_compute_address.app_ip.address}"
+  }
 }
 connection {
   type = "ssh"
@@ -48,4 +50,18 @@ resource "google_compute_firewall" "firewall_puma" {
 }
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["reddit-app"]
+}
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name = "default-allow-ssh"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+ }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "app_ip" {
+name = "reddit-app-ip"
 }
